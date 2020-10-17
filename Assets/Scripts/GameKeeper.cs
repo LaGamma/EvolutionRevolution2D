ec03fe61e4eeae129aa9b8bb9c094747426b7fab
Zach LaJoie <zachlajoie@Zachs-MacBook-Pro-4.local>
2020-10-17 00:45:56 -0400

    spawn creatures and food

---
+++ Assets/Scripts/GameKeeper.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameKeeper : MonoBehaviour
+{
+  public GameObject creature;
+  public GameObject food;
+  public float numCreatures;
+  public int numFood;
+  public float creatureRange;
+  public float foodRange;
+  public bool creatureSquare;
+  public bool foodSquare;
+  public bool randomDrop;
+  public bool includePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+      //settings
+      numCreatures = 10f;
+      numFood = 5;
+      creatureRange = 5f;
+      foodRange = 5f;
+      creatureSquare = false;
+      foodSquare = true;
+      includePlayer = true;
+
+      SpawnBatch(numCreatures);
+      SpawnFood(numFood);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      GameObject[] foodList = GameObject.FindGameObjectsWithTag("food");
+
+      if (foodList.Length == 0) SpawnFood(numFood);
+
+    }
+
+    public void SpawnBatch(float numCreatures) {
+      //spawns initial batch of creatures
+      Vector2 spawnPoint;
+      bool player;
+
+      for (int i=0; i<numCreatures; i++) {
+        if (creatureSquare) {
+          if (randomDrop) { //random square
+              spawnPoint = new Vector2(UnityEngine.Random.Range(-creatureRange, creatureRange),
+                                      UnityEngine.Random.Range(-creatureRange, creatureRange));
+            }
+          else {
+              float dist;
+              if (i < numCreatures/4) {
+                dist = -creatureRange + 2*creatureRange*(i / (numCreatures/4));
+                spawnPoint = new Vector2(-creatureRange, dist); }
+              else if (i < numCreatures/2) {
+                dist = creatureRange - 2*creatureRange*((i-(numCreatures/4)) / (numCreatures/4));
+                spawnPoint = new Vector2(creatureRange, dist); }
+              else if (i < 3*numCreatures/4) {
+                dist = -creatureRange + 2*creatureRange*((i-(numCreatures/2)) / (numCreatures/4));
+                spawnPoint = new Vector2(dist, creatureRange); }
+              else {
+                dist = creatureRange - 2*creatureRange*((i-(3*numCreatures/4)) / (numCreatures/4));
+                spawnPoint = new Vector2(dist, -creatureRange); }
+          }
+        } else { //square
+          if (randomDrop) { //random circle
+                spawnPoint = new Vector2(UnityEngine.Random.Range(-creatureRange, creatureRange),
+                                        UnityEngine.Random.Range(-creatureRange, creatureRange));
+                while (spawnPoint.magnitude > creatureRange) {
+                  spawnPoint = new Vector2(UnityEngine.Random.Range(-creatureRange, creatureRange),
+                                          UnityEngine.Random.Range(-creatureRange, creatureRange));
+                }
+            }
+           else { //circle
+              float angle = 2 * Mathf.PI * (i / numCreatures);
+              spawnPoint = new Vector2( creatureRange * Mathf.Cos(angle), creatureRange * Mathf.Sin(angle));
+            }
+          }
+
+          if (includePlayer &&
+              ((UnityEngine.Random.Range(0f,1f) <= (1f/numCreatures)) |
+                (i == (numCreatures - 1)))) {
+            player = true;
+            includePlayer = false;}
+          else {
+            player = false;
+          }
+
+          SpawnCreature(spawnPoint, player, new Dictionary<string,float>());
+      }
+    }
+
+    public void SpawnFood(int numFood) {
+      Vector2 spawnPoint;
+      Vector2 closestPoint;
+      for (int i=0; i<numFood; i++) {
+        do {
+          if (foodSquare) { //spawn food in square
+            spawnPoint = new Vector2(UnityEngine.Random.Range(-foodRange, foodRange),
+                                      UnityEngine.Random.Range(-foodRange, foodRange)); }
+          else { //spawn food in circle
+            float r = UnityEngine.Random.Range(0f, foodRange);
+            float theta = UnityEngine.Random.Range(0f, 2 * Mathf.PI);
+            spawnPoint = new Vector2(r*Mathf.Cos(theta),r*Mathf.Sin(theta));
+              }
+
+              closestPoint = FindClosestTag("creature").GetComponent<CircleCollider2D>().ClosestPoint(spawnPoint);
+          } while (food.GetComponent<BoxCollider2D>().bounds.Contains(closestPoint)) ;
+
+          Instantiate(food, spawnPoint, Quaternion.identity);
+      }
+  }
+
+  public GameObject FindClosestTag(string tag) {
+    GameObject[] tagList = GameObject.FindGameObjectsWithTag(tag);
+    GameObject tMin = null;
+    float min = Mathf.Infinity;
+    foreach (GameObject t in tagList) {
+      float dist = Vector3.Distance(t.transform.position, this.transform.position);
+      if (dist < min) {
+        tMin = t;
+        min = dist;
+      }
+    }
+    return tMin;
+  }
+
+  public void SpawnCreature(Vector2 sp, bool play, Dictionary<string,float> parent) {
+    GameObject simCreature = Instantiate(creature, sp, Quaternion.identity);
+    MoveCreature mc = simCreature.GetComponent<MoveCreature>();
+    mc.player = play;
+
+    if (parent.Count !=0) {
+        mc.SetGenetics(
+          new Dictionary<string,float> {
+            { "speed", RandNormal(parent["speed"], 0.2f, true) },
+            { "acceleration" , RandNormal(parent["acceleration"], 1f, true) },
+            { "vision", RandNormal(parent["vision"], 2f, true) }
+          }
+        );
+    }
+  }
+
+  public float RandNormal(float mean, float stdDev, bool pos) {
+    float val;
+    //set pos to true to only generate positive values
+    do {
+      //Box-Muller transform
+      float z1 = UnityEngine.Random.Range(0f,1f);
+      float z2 = UnityEngine.Random.Range(0f,1f);
+      float stdnorm = Mathf.Sqrt(-2 * Mathf.Log(z1)) *
+                       Mathf.Sin(2 * Mathf.PI * z2);
+      val = mean + stdDev * stdnorm; }
+      while (val < 0 | !pos);
+    return val;
+  }
+}
--- Assets/Scripts/MoveCreature.cs
+++ Assets/Scripts/MoveCreature.cs
@@ -4,39 +4,23 @@ using UnityEngine;
 
 public class MoveCreature : MonoBehaviour
 {
-  public bool player=true;
+  public bool player=false;
   public Dictionary<string,float> genetics;
-  public Vector3 lastPosition;
-  Vector3 acceleration = Vector3.zero;
-  Vector3 velocity = Vector3.zero;
-  Vector3 direction = Vector3.zero;
-  Vector3 colliderSize;
-  Rigidbody rb;
+  private Rigidbody2D rb;
   public bool dead=false;
 
     // Start is called before the first frame update
     void Start()
     {
+      rb = GetComponent<Rigidbody2D> ();
       if (genetics == null) {
       //creature attributes
         genetics = new Dictionary<string,float> {
           { "speed", RandNormal(1f, 0.2f, true) },
           { "acceleration" , RandNormal(5f, 1f, true) },
-          { "vision", RandNormal(10f, 2f, true) },
-          { "jumpSpeed", RandNormal(5f, 1f, true) },
-          { "volatility", RandNormal(0.01f, 0.1f, true) },
-          { "attack", RandNormal(100f, 20f, true) },
-          { "defense", RandNormal(100f, 20f, true) },
-          { "responsiveness", RandNormal(0.01f, 0.003f, true) }, //0=no forces towards other creatures,1=max
-          { "hunger", RandNormal(0.5f, 0.1f, true) }, //greater numbers=food is a motivating factor
-          { "aggression", RandNormal(0.5f, 0.1f, true) } //greater numbers=other creatures are a motivating factor
+          { "vision", RandNormal(10f, 2f, true) }
         };
       }
-
-      //get dimensions of creature
-      BoxCollider2D collider = GetComponent<BoxCollider2D>();
-      colliderSize = collider.size;
-      colliderSize = Vector3.Scale(colliderSize, transform.localScale);
     }
 
     // Update is called once per frame
@@ -44,64 +28,20 @@ public class MoveCreature : MonoBehaviour
     {
       //calculated acceleration direction (normalized)
       if (player) {
-          if (Input.GetKey(KeyCode.UpArrow)){
-            acceleration[1] = 1; }
-          else if (Input.GetKey(KeyCode.DownArrow)){
-            acceleration[1] = -1; }
-          else {
-            acceleration[1] = 0f;}
-
-          if (Input.GetKey(KeyCode.RightArrow)){
-            acceleration[0] = 1; }
-          else if (Input.GetKey(KeyCode.LeftArrow)){
-            acceleration[0] = -1; }
-          else {
-            acceleration[0] = 0; }
-
-          acceleration = acceleration.normalized;
+          float moveHorizontal = Input.GetAxis ("Horizontal");
+          float moveVertical = Input.GetAxis ("Vertical");
+          Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+          rb.AddForce (movement);
         }
       else { //Simulation creature
-        //if (GameObject.FindGameObjectsWithTag("food").Length != 0) {
           GameObject closestFood = FindClosestTag("food");
-          Vector3 foodPointer = closestFood.transform.position - transform.position;
 
-          Vector3 progress = transform.position - lastPosition;
-          progress = Vector3.Project(progress, acceleration);
-
-          Vector3 foodForce = Vector3.zero;
-
-          if (closestFood != null && Vector3.Magnitude(foodPointer) < genetics["vision"]) {
+          if (closestFood != null) {
             //add force towards nearest food (within vision)
-            foodForce = foodPointer.normalized;
+            Vector2 foodPointer = closestFood.transform.position - transform.position;
+            rb.AddForce (foodPointer.normalized);
           }
-
-          if (acceleration == Vector3.zero ) {
-            //redirect acceleration
-            print(gameObject.name + " rerouting");
-            float angle = UnityEngine.Random.Range(0,(float) Mathf.PI*2);
-            acceleration = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle));
-          }
-          acceleration = acceleration.normalized * genetics["acceleration"];
         }
-
-        //update velocity
-        velocity += acceleration * genetics["acceleration"] * Time.deltaTime;
-
-        //clamp to maximum speed
-        velocity = Vector3.ClampMagnitude(velocity, genetics["speed"]);
-
-        //update position
-        lastPosition = transform.position;
-        transform.position += (velocity * Time.deltaTime);
-
-        //look in the direction you're moving
-        // if (velocity != Vector3.zero) {
-        //   direction = velocity.normalized;
-        //   transform.rotation = Quaternion.Slerp(transform.rotation,
-        //                                         Quaternion.LookRotation(direction), Time.deltaTime);
-        // }
-
-        Vector3 p = transform.position;
     }
 
     public GameObject FindClosestTag(string tag) {
@@ -120,8 +60,15 @@ public class MoveCreature : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col) {
       if (col.gameObject.tag == "food") {
-        Destroy(col.gameObject);
-      print("eat"); }
+        Destroy(col.gameObject); }
+      if (col.gameObject.tag == "background" && player == false) {
+        float angle = UnityEngine.Random.Range(0,(float) Mathf.PI*2);
+        rb.AddForce (new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)));
+        }
+    }
+
+    public void SetGenetics(Dictionary<string,float> jeans) {
+      genetics = jeans;
     }
 
     public float RandNormal(float mean, float stdDev, bool pos) {
